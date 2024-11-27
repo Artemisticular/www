e1fe29700df646a5cbfc0963e11fb1bbafd0ae6b
Artemisticular <artemisticular@gmail.com>
2024-11-27 20:27:40 +0700

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [fileName, setFileName] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFileName(event.target.files[0].name);
+    }
+  };
+
   return (
-    <div></div>
+    <div>
+      <h1>Upload File</h1>
+      <input type="file" onChange={handleFileChange} />
+      {fileName && <p>File yang diunggah: {fileName}</p>}
+    </div>
   );
 }
